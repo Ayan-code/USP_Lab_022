d54aa462afa51843f177366ddf6e877b4fdbd91f
Ayan-Decoder <53806596+Ayan-code@users.noreply.github.com>
2020-10-08 02:47:39 -0700

    Add files via upload

---
+++ Lab3/num.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+echo "Enter a number"
+read x
+y=0
+
+if [ $x -eq $y ]
+then
+echo "Number is zero"
+elif [ $x -gt $y ]
+then
+echo "Number is positive."
+else
+echo "Number is negative."
+fi
